3e7f92fe8a51bdd1ddfb6153682b392ca83e6de3
InsanityAI <insanityaiiii@gmail.com>
2025-04-20 20:19:32 +0200

    fix: metadata generator

--- generateProgramMetadata.js
+++ generateProgramMetadata.js
@@ -1,8 +1,17 @@
 const pkg = require('./package.json')
+const fs = require('node:fs');
 
-console.log(`export const NAME = '${pkg.name}'
+const content = `export const NAME = '${pkg.name}'
 export const DESCRIPTION = '${pkg.description}'
 export const DISPLAY_NAME = '${pkg.displayName}'
 export const VERSION = '${pkg.version}'
 export const AUTHOR = '${pkg.author}'
-export const LICENSE = '${pkg.license}'`)
+export const LICENSE = '${pkg.license}'`;
+
+fs.writeFile('src/metadata.ts', content, err => {
+  if (err) {
+    console.error(err);
+  } else {
+    // done!
+  }
+});
